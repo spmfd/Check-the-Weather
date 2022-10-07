3aa03d4c69ef168f788a3030f299bb0734fdd1f5
spmfd <110492974+spmfd@users.noreply.github.com>
2022-10-06 18:29:55 -0700

    Search history created with help of Tutor Sangeetha

--- assets/javascript.js
+++ assets/javascript.js
@@ -2,20 +2,33 @@ const todaycity = document.getElementById('todays-weather-card')
 const citySearchBtn = document.getElementById('city-search')
 const forecastcity = document.getElementById('card-container')
 const forecastdate = document.getElementById('forecast-date')
+const clearhist = document.getElementById('clear')
 
-citySearchBtn.addEventListener('click', function(){
-    let searchInputTxt = document.getElementById('search-input').value.trim();
-    weather(searchInputTxt)
-    forecastweather(searchInputTxt)
-    searchhistory(searchInputTxt)
+citySearchBtn.addEventListener('click', function () {
+        let searchInputTxt = document.getElementById('search-input').value.trim();
+        weather(searchInputTxt)
+        forecastweather(searchInputTxt)
+        searchhistory(searchInputTxt)
 })
 
-function weather(searchInputTxt){
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchInputTxt}&appid=93ca7c98e96312611f46df22df330813&units=imperial`)
-    .then(response => response.json())
-    .then(data => {
-        let html = "";
-        html += `
+function prevcitySearch(event) {
+        console.log(event.target.textContent)
+        let searchcity = event.target.textContent;
+        weather(searchcity)
+        forecastweather(searchcity)
+}
+
+clearhist.addEventListener('click',function(){
+         localStorage.removeItem("city")
+         document.getElementById("previous-search").innerHTML =""
+})
+
+function weather(searchInputTxt) {
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${searchInputTxt}&appid=93ca7c98e96312611f46df22df330813&units=imperial`)
+                .then(response => response.json())
+                .then(data => {
+                        let html = "";
+                        html += `
                 <h2 id="currentCity">
                 ${searchInputTxt} - ${moment().format('MMM Do YY')}- <img src="https://openweathermap.org/img/wn/${data.weather[0].icon}.png">
                 </h2>
@@ -24,64 +37,49 @@ function weather(searchInputTxt){
                 <p>Humidity: ${data.main.humidity} %</p>
                 </div>
         `;
-        // console.log(data.weather[0].icon)
-        todaycity.innerHTML = html;
-        });
+                        // console.log(data.weather[0].icon)
+                        todaycity.innerHTML = html;
+                        let search = JSON.parse(localStorage.getItem("city")) || []
+                        if (search.indexOf(searchInputTxt) === -1) {
+                                search.push(searchInputTxt)
+                                localStorage.setItem("city", JSON.stringify(search))
+                                displaycity()
+                        }
+                });
+
 };
-    
 
-function forecastweather(searchInputTxt){
-    fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${searchInputTxt}&appid=93ca7c98e96312611f46df22df330813&units=imperial`)
-    .then(response => response.json())
-    .then(data => {
-        let html = "";
-             html += `
+displaycity()
+function forecastweather(searchInputTxt) {
+        fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${searchInputTxt}&appid=93ca7c98e96312611f46df22df330813&units=imperial`)
+                .then(response => response.json())
+                .then(data => {
+                        let html = "";
+                        let t=1; 
+                        for(let i=0;i<data.list.length;i=i+8){
+                        html += `
         <div class="future-card">
-                    <h3 id="forecast-date">${moment().add(1, 'days').format('MMM Do YY')}</h3>
+                    <h3 id="forecast-date">${moment().add(t, 'days').format('MMM Do YY')}</h3>
                     <p></p>
-                    <p><img src="https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}.png"></p>
-                    <p>Temperature: ${data.list[0].main.temp} °F</p>
-                    <p>Wind Speed: ${data.list[0].wind.speed} MPH</p>
-                    <p>Humidity: ${data.list[0].main.humidity} %</p>
-        </div>
-        <div class="future-card">
-                <h3 id="forecast-date">${moment().add(2, 'days').format('MMM Do YY')}</h3>
-                <p></p>
-                <p><img src="https://openweathermap.org/img/wn/${data.list[1].weather[0].icon}.png"></p>
-                <p>Temperature: ${data.list[1].main.temp} °F</p>
-                <p>Wind Speed: ${data.list[1].wind.speed} MPH</p>
-                <p>Humidity: ${data.list[1].main.humidity} %</p>
-        </div>
-        <div class="future-card">
-                <h3 id="forecast-date">${moment().add(3, 'days').format('MMM Do YY')}</h3>
-                <p></p>
-                <p><img src="https://openweathermap.org/img/wn/${data.list[2].weather[0].icon}.png"></p>
-                <p>Temperature: ${data.list[2].main.temp} °F</p>
-                <p>Wind Speed: ${data.list[2].wind.speed} MPH</p>
-                <p>Humidity: ${data.list[2].main.humidity} %</p>
-        </div>
-        <div class="future-card">
-                <h3 id="forecast-date">${moment().add(4, 'days').format('MMM Do YY')}</h3>
-                <p></p>
-                <p><img src="https://openweathermap.org/img/wn/${data.list[3].weather[0].icon}.png"></p>
-                <p>Temperature: ${data.list[3].main.temp} °F</p>
-                <p>Wind Speed: ${data.list[3].wind.speed} MPH</p>
-                <p>Humidity: ${data.list[3].main.humidity} %</p>
-        </div>
-        <div class="future-card">
-                <h3 id="forecast-date">${moment().add(5, 'days').format('MMM Do YY')}</h3>
-                <p></p>
-                <p><img src="https://openweathermap.org/img/wn/${data.list[4].weather[0].icon}.png"></p>
-                <p>Temperature: ${data.list[4].main.temp} °F</p>
-                <p>Wind Speed: ${data.list[4].wind.speed} MPH</p>
-                <p>Humidity: ${data.list[4].main.humidity} %</p>
-        </div>
-        `;
-            forecastcity.innerHTML = html;
-        })
+                    <p><img src="https://openweathermap.org/img/wn/${data.list[i].weather[0].icon}.png"></p>
+                    <p>Temperature: ${data.list[i].main.temp} °F</p>
+                    <p>Wind Speed: ${data.list[i].wind.speed} MPH</p>
+                    <p>Humidity: ${data.list[i].main.humidity} %</p>
+        </div>`
+                   t++;     
+        ;}
+                        forecastcity.innerHTML = html;
+                })
 }
 
-function searchhistory(searchInputTxt){
-    localStorage.setItem("search", searchInputTxt);
-    document.getElementById("previous-search").innerHTML = localStorage.getItem("search");
+function displaycity() {
+        let search = JSON.parse(localStorage.getItem("city")) || []
+        let htmlCode = ""
+        for (let i = 0; i < search.length; i++) {
+                htmlCode += `<li><button class="search">${search[i]}</button></li>`
+        }
+        document.getElementById("previous-search").innerHTML = htmlCode;
+        let searchButtons = document.querySelectorAll(".search")
+        searchButtons.forEach(element => element.addEventListener("click", prevcitySearch))
 }
+
